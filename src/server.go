c1defa8163baf926766ff7af9ac35ab87d8ca043
Shun Yu <shunsmailbox@gmail.com>
2020-01-21 01:53:24 -0800

    Parser

--- src/server.go
+++ src/server.go
@@ -3,17 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/ishunyu/magpie-dict/parser"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
+var fileA = "C:\\Users\\Shun\\Documents\\code\\magpie-dict\\resource\\subtitles\\ep21 - a.sbv"
 
+// var fileB = "C:\\Users\\Shun\\Documents\\code\\magpie-dict\\resource\\subtitles\\ep21 - a.sbv"
+
+func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, "Hello World!")
 }
 
-func main() {
-	http.HandleFunc("/hello", hello)
+func parse(w http.ResponseWriter, req *http.Request) {
+	res, _ := parser.ParseSBV(fileA)
+	fmt.Fprintln(w, res)
+}
 
-	port := 8090
-	fmt.Printf("Starting server on port %v...", port)
-	http.ListenAndServe(fmt.Sprint(":", port), nil)
+func parsee() {
+	f, _ := parser.ParseSBV(fileA)
+	fmt.Println(f)
+}
+
+func main() {
+	// http.HandleFunc("/hello", hello)
+	// http.HandleFunc("/parse", parse)
+
+	// port := 8090
+	// fmt.Printf("Starting server on port %v...", port)
+	// http.ListenAndServe(fmt.Sprint(":", port), nil)
+
+	parsee()
 }
